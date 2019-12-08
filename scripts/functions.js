c0ee2e87e88aeb2bd5631b2396f4f4289c1b1ece
LHanji <lohana100739@gmail.com>
2019-12-08 14:41:06 -0300

    Graph 2

--- scripts/functions.js
+++ scripts/functions.js
@@ -18,9 +18,28 @@ var drawGrap = function(){
         return chart;
       });           
 }
-$(function(){
-    $("#loadGrap").click(function(){
-        console.log('draw grap');
-        drawGrap();       
-    });
-});
+var drawGrap2 = function(){
+    nv.addGraph(function() {
+        var chart = nv.models.discreteBarChart()
+          .x(function(d) { return d.label })
+          .y(function(d) { return d.value })
+          .staggerLabels(true)
+          .tooltips(false)
+          .showValues(true)
+      
+        d3.select('#chart2 svg')
+          .datum(grapData)
+          .transition().duration(500)
+          .call(chart)
+          ;
+      
+        nv.utils.windowResize(chart.update);
+      
+        return chart;
+      });           
+}
+function loadGraps(){
+    console.log('draw grap');
+    drawGrap();
+    drawGrap2();       
+}
