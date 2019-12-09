d4bb880308153c29d7463ca24752f3cdfb8c0219
LHanji <lohana100739@gmail.com>
2019-12-08 22:19:27 -0300

    Gráfico com BD

--- server.php
+++ server.php
@@ -11,36 +11,25 @@
     }
 
     if(isset($_POST['enviar'])){
-        $_POST['Red Dead Redemption 2'] = ( isset($_POST['Red Dead Redemption 2']) ) ? $nomejogo = $_POST['Red Dead Redemption 2'] : null;
-        $_POST['Call of Duty Modern Warfare'] = ( isset($_POST['Call of Duty Modern Warfare']) ) ? $nomejogo = $_POST['Call of Duty Modern Warfare'] : null;
-        $_POST['GTA 5'] = ( isset($_POST['GTA 5']) ) ? $nomejogo = $_POST['GTA 5'] : null;
-        $_POST['Star Wars Jedi Fallen Order'] = ( isset($_POST['Star Wars Jedi Fallen Order']) ) ? $nomejogo = $_POST['Star Wars Jedi Fallen Order'] : null;
-        $_POST['Fortnite'] = ( isset($_POST['Fortnite']) ) ? $nomejogo = $_POST['Fortnite'] : null;
-        $_POST['League of Legends'] = ( isset($_POST['League of Legends']) ) ? $nomejogo = $_POST['League of Legends'] : null;
-        $_POST['CS GO'] = ( isset($_POST['CS GO']) ) ? $nomejogo = $_POST['CS GO'] : null;
-        $_POST['PUBG Lite'] = ( isset($_POST['PUBG Lite']) ) ? $nomejogo = $_POST['PUBG Lite'] : null;
-        $_POST['PUBG'] = ( isset($_POST['PUBG']) ) ? $nomejogo = $_POST['PUBG'] : null;
-        $_POST['Minecraft'] = ( isset($_POST['Minecraft']) ) ? $nomejogo = $_POST['Minecraft'] : null;
-        
-        $query = "UPDATE jogos SET valor += 1 WHERE nomejogo = '$nomejogo'";
-        mysqli_query($conn, $query);
+        $nomejogo = $_POST['nomejogo'];
 
-        unlink('data.csv');
-        fopen('data.csv','w+');
-        fclose('data.csv');
+        $query = "UPDATE jogos SET valor += '1' WHERE nomejogo = '$nomejogo'";
+        
+        mysqli_query($conn, $query);
 
         $file = fopen("data.csv", "w");
 
         $query2 = "SELECT * FROM jogos";
+        $result = mysqli_query($conn, $query2);
 
-        while($row = mysqli_fetch_assoc($query2))
+        fputcsv($file, array("label","value"));
+        while($row = mysqli_fetch_assoc($result))
         {
-            $nomejogo = $row['jogos'];
-            $total = $row['valor'];
-
-            fputcsv($file, array($nomejogo, $total));
+            fputcsv($file, $row);
         }
 
         fclose($file);
+
+        header('location: funcionou.html');
     }
 ?>
